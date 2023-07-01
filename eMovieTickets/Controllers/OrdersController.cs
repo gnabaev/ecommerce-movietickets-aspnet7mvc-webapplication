070aa858fe42d0a584ea208b00e326985f0af4f3
gnab <gnabaev@gmail.com>
2023-07-01 17:47:45 +0300

    Addition of the Orders controller and Shopping Cart viewmodel.

---
+++ eMovieTickets/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using eMovieTickets.Data.Cart;
+using eMovieTickets.Data.Services;
+using eMovieTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMovieTickets.Controllers
+{
+	public class OrdersController : Controller
+	{
+		private readonly IMoviesService _moviesService;
+		private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+			_shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+		{
+			var items = _shoppingCart.GetShoppingCartItems();
+			_shoppingCart.ShoppingCartItems = items;
+
+			var response = new ShoppingCartVM()
+			{
+				ShoppingCart = _shoppingCart,
+				ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal(),
+			};
+
+			return View(response);
+		}
+	}
+}
---
+++ eMovieTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,11 @@
+﻿using eMovieTickets.Data.Cart;
+
+namespace eMovieTickets.Data.ViewModels
+{
+	public class ShoppingCartVM
+	{
+		public ShoppingCart ShoppingCart { get; set; }
+
+		public double ShoppingCartTotal { get; set; }
+	}
+}
