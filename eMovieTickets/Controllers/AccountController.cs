e30b26338ca811a766519163de4d26aca6440c38
gnab <gnabaev@gmail.com>
2023-07-04 21:53:02 +0300

    Creation of the method to log in user.

--- eMovieTickets/Controllers/AccountController.cs
+++ eMovieTickets/Controllers/AccountController.cs
@@ -19,11 +19,46 @@ namespace eMovieTickets.Controllers
 			_context = context;
         }
 
+		
         public IActionResult Login()
 		{
 			var response = new LoginVM();
 
 			return View(response);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginVM loginVM)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(loginVM);
+			}
+
+			var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+
+			if (user != null)
+			{
+				var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+
+				if (passwordCheck)
+				{
+					var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+					if (result.Succeeded)
+					{
+						return RedirectToAction("Index", "Movies");
+					}
+				}
+
+				TempData["Error"] = "Wrong credentials. Please, try again!";
+
+				return View(loginVM);
+			}
+
+			TempData["Error"] = "Wrong credentials. Please, try again!";
+
+			return View(loginVM);
 		}
 	}
 }
