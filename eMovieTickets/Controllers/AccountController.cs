0ddcdd24a64d1a066f192ff1c27b3acdc2664f22
gnab <gnabaev@gmail.com>
2023-07-04 20:30:30 +0300

    Creation of the Account controller.

---
+++ eMovieTickets/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using eMovieTickets.Data;
+using eMovieTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMovieTickets.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly  AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_context = context;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
